eae31d61459b4d6b11c70b04bb17a78adb56efb1
Alon Niv <oakfang@gmail.com>
2017-01-23 05:58:50 +0200

    0.2.0

--- index.js
+++ index.js
@@ -1,163 +1,95 @@
-'use strict';
+const EventEmitter = require('events');
+const Peer = require('simple-peer');
+const WebSocket = require('ws');
+const wrtc = require('wrtc');
+const uuid = require('uuid');
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+const getOptions = ({
+  peerSpec=null,
+  signaling='ws://localhost:3000',
+  room='default',
+  maxPeers=Infinity,
+}={}) => ({
+  peerSpec: Object.assign({ wrtc }, peerSpec || {}),
+  signaling,
+  room,
+  maxPeers,
+});
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var EventEmitter = require('events');
-var Peer = require('simple-peer');
-var WebSocket = require('ws');
-var uuid = require('uuid');
-
-var getOptions = function getOptions() {
-  var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
-      _ref$peerSpec = _ref.peerSpec,
-      peerSpec = _ref$peerSpec === undefined ? null : _ref$peerSpec,
-      _ref$signaling = _ref.signaling,
-      signaling = _ref$signaling === undefined ? 'ws://localhost:3000' : _ref$signaling,
-      _ref$room = _ref.room,
-      room = _ref$room === undefined ? 'default' : _ref$room;
-
-  return {
-    peerSpec: peerSpec,
-    signaling: signaling,
-    room: room
-  };
-};
-
-var getSignalingServer = function getSignalingServer(signaling) {
+const getSignalingServer = signaling => {
   if (typeof signaling === 'string') {
     return new WebSocket(signaling);
   }
   return signaling;
 };
 
-var Coven = function (_EventEmitter) {
-  _inherits(Coven, _EventEmitter);
+class Coven extends EventEmitter {
+  constructor(options) {
+    super();
+    this.peers = new Map();
+    this.id = uuid();
+    const { peerSpec, signaling, room, maxPeers } = getOptions(options);
+    this.room = room;
+    this.spec = peerSpec;
+    this.server = getSignalingServer(signaling);
 
-  function Coven(options) {
-    _classCallCheck(this, Coven);
-
-    var _this = _possibleConstructorReturn(this, (Coven.__proto__ || Object.getPrototypeOf(Coven)).call(this));
-
-    _this.peers = new Map();
-    _this.id = uuid();
-
-    var _getOptions = getOptions(options),
-        peerSpec = _getOptions.peerSpec,
-        signaling = _getOptions.signaling,
-        room = _getOptions.room;
-
-    _this.room = room;
-    _this.spec = peerSpec;
-    _this.server = getSignalingServer(signaling);
-
-    _this.server.on('open', function () {
-      _this._signal('UP', _this.id, null, true);
-      _this.emit('connected');
+    this.server.on('open', () => {
+      this._signal('UP', this.id, null, true);
+      this.emit('connected');
     });
 
-    _this.server.on('message', function (msg) {
-      var _JSON$parse = JSON.parse(msg),
-          type = _JSON$parse.type,
-          origin = _JSON$parse.origin,
-          target = _JSON$parse.target,
-          data = _JSON$parse.data;
-
-      if (origin === _this.id) return;
-      _this.emit('signal', { type: type, origin: origin, target: target, data: data });
+    this.server.on('message', msg => {
+      const { type, origin, target, data } = JSON.parse(msg);
+      if (origin === this.id) return;
+      this.emit('signal', {type, origin, target, data});
       switch (type) {
-        case 'UP':
-          {
-            if (!_this.peers.has(origin)) {
-              _this.peers.set(origin, _this._getPeer(origin, data));
-              if (data) {
-                _this._signal('UP', _this.id, origin, false);
-              }
+        case 'UP': {
+          if ((!this.peers.has(origin)) && this.peers.size < maxPeers) {
+            this.peers.set(origin, this._getPeer(origin, data));
+            if (data) {
+              this._signal('UP', this.id, origin, false);
             }
-            return;
           }
-        case 'SIGNAL':
-          {
-            if (target !== _this.id) return;
-            if (!_this.peers.has(origin)) {
-              _this.peers.set(origin, _this._getPeer(origin));
-            }
-            _this.peers.get(origin).signal(data);
-            return;
-          }
+          return;
+        }
+        case 'SIGNAL': {
+          if (target !== this.id) return;
+          this.peers.get(origin).signal(data);
+          return;
+        }
       }
     });
-    return _this;
   }
 
-  _createClass(Coven, [{
-    key: '_signal',
-    value: function _signal(type, origin, target, data) {
-      var room = this.room;
-      this.server.send(JSON.stringify({
-        type: type,
-        room: room,
-        origin: origin,
-        target: target,
-        data: data
-      }));
-    }
-  }, {
-    key: '_getPeer',
-    value: function _getPeer(id, initiator) {
-      var _this2 = this;
+  _signal(type, origin, target, data) {
+    const room = this.room;
+    this.server.send(JSON.stringify({
+      type,
+      room,
+      origin,
+      target,
+      data,
+    }));
+  }
 
-      var peer = new Peer(Object.assign({
-        initiator: initiator
-      }, this.spec || {}));
-      peer.covenId = id;
-      peer.on('signal', function (data) {
-        return _this2._signal('SIGNAL', _this2.id, id, data);
-      });
-      peer.once('close', function () {
-        return _this2.peers.delete(id);
-      });
-      peer.once('connect', function () {
-        return _this2.emit('peer', peer);
-      });
-      return peer;
-    }
-  }, {
-    key: 'broadcast',
-    value: function broadcast(data) {
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
+  _getPeer(id, initiator) {
+    const peer = new Peer(Object.assign({
+      initiator,
+    }, this.spec));
+    peer.covenId = id;
+    peer.on('signal', data => this._signal('SIGNAL', this.id, id, data));
+    peer.once('close', () => this.peers.delete(id));
+    peer.once('connect', () => this.emit('peer', peer));
+    return peer;
+  }
 
-      try {
-        for (var _iterator = this.peers.values()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var peer = _step.value;
-
-          peer.send(data);
-        }
-      } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion && _iterator.return) {
-            _iterator.return();
-          }
-        } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
-          }
-        }
+  broadcast(data) {
+    for (const peer of this.peers.values()) {
+      if (peer.connected) {
+        peer.send(data);
       }
     }
-  }]);
-
-  return Coven;
-}(EventEmitter);
+  }
+}
 
 module.exports = Coven;
