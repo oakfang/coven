ed9cd16152e8290d93813f2941feeb1983344034
Alon Niv <alon.n@seebo.com>
2018-04-10 09:00:03 +0300

    Fix major rooms bug

--- server.js
+++ server.js
@@ -1,37 +1,75 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
+
+class Room {
+  constructor() {
+    this._peers = new Map();
+    this._socks = new WeakMap();
+  }
+
+  get size() {
+    return this._peers.size;
+  }
+
+  hasPeer(peerId) {
+    return this._peers.has(peerId);
+  }
+
+  addPeer(socket, peerId) {
+    this._peers.set(peerId, socket);
+    this._socks.set(socket, peerId);
+  }
+
+  removePeer(socket) {
+    this._peers.delete(this._socks.get(socket));
+  }
+
+  broadcast(data) {
+    for (let socket of this._peers.values()) {
+      if (socket.readyState === WebSocket.OPEN) {
+        socket.send(data);
+      }
+    }
+  }
+
+  sendTo(peerId, data) {
+    const socket = this._peers.get(peerId);
+    if (socket) {
+      socket.send(data);
+    }
+  }
+}
 
 function getServer(config, onMessage) {
   Object.assign(config || {}, {
     clientTracking: true
   });
   const wss = new WebSocket.Server(config);
-  const peers = new Map();
+
+  const roomBySocket = new WeakMap();
   const rooms = {};
-  const broadcast = data => Array.from(wss.clients)
-                                 .filter(({ readyState }) =>
-                                    readyState === WebSocket.OPEN)
-                                 .forEach(client => client.send(data));
-  wss.on('connection', ws => {
-    ws.on('close', () => {
-      if (peers.has(ws)) {
-        const { room, id } = peers.get(ws);
-        rooms[room].delete(ws);
-        if (rooms[room].size === 0) {
-          delete rooms[room];
+
+  wss.on("connection", ws => {
+    ws.on("close", () => {
+      const roomName = roomBySocket.get(ws);
+      if (roomName) {
+        const room = rooms[roomName];
+        room.removePeer(ws);
+        if (room.size === 0) {
+          delete rooms[roomName];
         }
       }
     });
-    ws.on('message', msg => {
-      const { room, origin, target, type, data } = JSON.parse(msg);
-      onMessage && onMessage({ room, origin, target, type, data });
-      if (!rooms[room]) rooms[room] = new Map();
-      const ids = rooms[room];
-      if (!ids.has(origin)) {
-        ids.set(origin, ws);
-        peers.set(ws, { room, id: origin });
+    ws.on("message", msg => {
+      const { room: roomName, origin, target, type, data } = JSON.parse(msg);
+      onMessage && onMessage({ room: roomName, origin, target, type, data });
+      if (!rooms[roomName]) rooms[roomName] = new Room();
+      const room = rooms[roomName];
+      if (!room.hasPeer(origin)) {
+        room.addPeer(ws, origin);
+        roomBySocket.set(ws, roomName);
       }
-      if (!target) return broadcast(msg);
-      return ids.get(target).send(msg);
+      if (!target) return room.broadcast(msg);
+      return room.sendTo(target, msg);
     });
   });
   return wss;
